2a6fb71ee1d137f3ad247f20b1ad7a6e3819f01e
Muhammad Humayun <humayun@MacBook-Pro.local>
2022-06-27 18:03:18 -0400

    Added port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals');
 const express = require('express');
 const app = express();
@@ -46,5 +47,5 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
